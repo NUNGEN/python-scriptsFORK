fec7ec22cf8401949aaccf2d1dbdcf4837b99927
NUNGEN <nikita.voronin0211@gmail.com>
2025-05-15 11:32:31 +0300

    Add files via upload

---
+++ weather_request.py
@@ -0,0 +1,9 @@
+api_key = "2d76fdaa7e7b3f369a9010894092ada4"
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Put your link")
+
+params = {
+    "IPs":api_key,S
+    }
+request.get(url,city)
